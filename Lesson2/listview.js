4e7adbd645a222dd63ec498dca332cf323d49e28
zhangjie <zhangjie4@9fbank.cc>
2016-09-20 19:20:12 +0800

    增加初始数据

--- Lesson2/listview.js
+++ Lesson2/listview.js
@@ -16,9 +16,8 @@ import SectionComponent from "./section.js";
 export default class ListViewComponent extends Component {
     
     static defaultProps = {
-        names: {
-            
-        }
+        sections: ['section 1', 'section 2', 'section 3', 'section 4'],
+        rows: ['row 1', 'row 2', 'row 3', 'row 4', 'row 5', 'row 6'],
     };
     
     // 构造
@@ -28,16 +27,14 @@ export default class ListViewComponent extends Component {
         var getSectionData = (dataBlob, sectionId) => dataBlob[sectionId];
         var getRowData = (dataBlob, sectionId, rowId) => dataBlob[sectionId + ":" + rowId];
         
-        const ds = new ListView.DataSource({
-            getSectionData: getSectionData,
-            getRowData: getRowData,
-            rowHasChanged: (r1, r2) => r1 !== r2,
-            sectionHeaderHasChanged: (s1, s2) => s1 !== s2,
-        });
         // 初始状态
         this.state = {
-            dataSource: ds.cloneWithRows(this.props.names),
-            
+            dataSource: new ListView.DataSource({
+                getSectionData: getSectionData,
+                getRowData: getRowData,
+                rowHasChanged: (r1, r2) => r1 !== r2,
+                sectionHeaderHasChanged: (s1, s2) => s1 !== s2,
+            }),
         };
     }
     
@@ -46,15 +43,31 @@ export default class ListViewComponent extends Component {
             <ListView
                 style={styles.list}
                 dataSource={this.state.dataSource}
-                renderSectionHeader={(sectionData, sectionId) => <SectionComponent data={sectionData} />}
-                renderRow={(rowData, sectionId, rowId) => <RowComponent data={rowData} press={this._onPress} />}
+                renderSectionHeader={(sectionData, sectionId) => <SectionComponent data={sectionData + " " + sectionId} />}
+                renderRow={(rowData, sectionId, rowId) => <RowComponent data={rowData + " " + sectionId + " " + rowId} press={this._onPress} />}
             />
         );
+    }
+    
+    componentWillMount() {
+        this._fakeData();
     }
     
     _onPress(data) {
         alert(data);
     }
+    
+    _fakeData() {
+        var dataBlob = {};
+        var sectionIds = [];
+        var rowIds = [];
+        
+        this.props.sections.map((data) => {
+            
+        });
+        
+    }
+    
 }
 
 const styles = StyleSheet.create({
